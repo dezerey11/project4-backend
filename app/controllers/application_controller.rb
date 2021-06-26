dec89593c18e5b73ba49827b83825d98e186a35f
Dezerey Escanuelas <dezerey11@gmail.com>
2021-06-25 17:01:49 -0700

    completed authentication setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,44 @@
 class ApplicationController < ActionController::API
+  # run this function before hitting any routes in the controller
+  before_action :authorized
+
+  def encode_token(payload)
+    JWT.encode(payload, 'yourSecret')
+  end
+
+  def auth_header
+    # { Authorization: 'Bearer <token>' }
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      # header: { 'Authorization': 'Bearer <token>' }
+      begin
+        JWT.decode(token, 'yourSecret', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def logged_in_user
+    # if token is not null
+    if decoded_token
+      # get user id and decode the token
+      user_id = decoded_token[0]['user_id']
+      # looks for user and assigns it to the property
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  #returns a boolean based on what logged_in_user returns
+  def logged_in?
+    !!logged_in_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
 class UsersController < ApplicationController
+  #run this before the route. Check if it is authorized. Only use it for auto_login route
+  before_action :authorized, only: [:auto_login]
+
+  # REGISTER
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: "Invalid username or password"}
+    end
+  end
+
+  # LOGGING IN
+  def login
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: "Invalid username or password"}
+    end
+  end
+
+
+  def auto_login
+    render json: @user
+  end
+
+  private
+  # grab params 
+  def user_params
+    params.permit(:username, :password)
+  end
+
 end
